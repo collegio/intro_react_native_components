c6122b6bc3b4bbd6c5c1c0ff1e0973c6da133ac6
Robert Myers <robertmyers@Roberts-MacBook-Air.local>
2018-04-10 00:45:50 -0300

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends React.Component {
+    render() {
+        return (
+            <View style={{marginTop: 200}}>
+                <Text>Let's start our code here!</Text>
+            </View>
+        );
+    }
+};
+
+export default App;
